4d039e6ca605f8675b9ef6b5868dc64a2029e6c4
allan victor <allan_victor98@hotmail.com>
2022-05-03 18:14:02 -0300

    funcionalidades de acessibilidade

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,27 +1,26 @@
 import { ChatTeardropText } from 'phosphor-react'
 import { useState } from 'react';
+import { Popover } from '@headlessui/react'
 
 
 export function Widget(){
-    const [visibilityWidget,setVisibilityWidget] = useState(false)
-
-    function changeVisibility(){
-        setVisibilityWidget(!visibilityWidget)
-       
-    }
-
+   
 
     return(
         <>
-            <div className="absolute right-4 bottom-4">
-                { visibilityWidget && <h1>allan</h1> }
-                <button onClick={changeVisibility} className="bg-fuchsia-900 text-white p-3 rounded-full flex items-center group">
+            <Popover className="absolute right-4 bottom-4">
+                
+                <Popover.Panel>
+                    <h1>allan</h1>
+                </Popover.Panel>
+
+                <Popover.Button  className="bg-fuchsia-900 text-white p-3 rounded-full flex items-center group">
                     <ChatTeardropText size={40} />
                     <span className="max-w-0 overflow-hidden group-hover:max-w-xs ease-linear duration-300">
                         Feedback
                     </span>
-                </button>
-            </div>
+                </Popover.Button>
+            </Popover>
         </>
     );
 }
